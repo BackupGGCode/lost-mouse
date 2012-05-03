e9b69812d62c668cbf50d1ef99e46570b65ef0fe
kamil <kamil@r830>
2012-05-03 17:35:39 +0200

    początkowy projekt ecpipsa z działającym opencv

---
+++ Default/dft.d
@@ -0,0 +1,31 @@
+dft.d dft.o: ../dft.cpp \
+ C:\opencv_2.4.0_mingw32\include/opencv2/core/core.hpp \
+ C:\opencv_2.4.0_mingw32\include/opencv2/core/types_c.h \
+ C:\opencv_2.4.0_mingw32\include/opencv2/core/version.hpp \
+ C:\opencv_2.4.0_mingw32\include/opencv2/core/operations.hpp \
+ C:\opencv_2.4.0_mingw32\include/opencv2/core/mat.hpp \
+ C:\opencv_2.4.0_mingw32\include/opencv2/imgproc/imgproc.hpp \
+ C:\opencv_2.4.0_mingw32\include/opencv2/imgproc/types_c.h \
+ C:\opencv_2.4.0_mingw32\include/opencv2/core/core_c.h \
+ C:\opencv_2.4.0_mingw32\include/opencv2/highgui/highgui.hpp \
+ C:\opencv_2.4.0_mingw32\include/opencv2/highgui/highgui_c.h
+
+C:\opencv_2.4.0_mingw32\include/opencv2/core/core.hpp:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/core/types_c.h:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/core/version.hpp:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/core/operations.hpp:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/core/mat.hpp:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/imgproc/imgproc.hpp:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/imgproc/types_c.h:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/core/core_c.h:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/highgui/highgui.hpp:
+
+C:\opencv_2.4.0_mingw32\include/opencv2/highgui/highgui_c.h:
