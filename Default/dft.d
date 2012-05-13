8d2905f3c89a4bf9185e0c07a941877e984a36dd
kamil <kamil@r830>
2012-05-13 14:55:13 +0200

    przejście na opencv 2.3.1 zmiana katalogu opencv na C:opencv

--- Default/dft.d
+++ Default/dft.d
@@ -1,31 +1,30 @@
-dft.d dft.o: ../dft.cpp \
- C:\opencv_2.4.0_mingw32\include/opencv2/core/core.hpp \
- C:\opencv_2.4.0_mingw32\include/opencv2/core/types_c.h \
- C:\opencv_2.4.0_mingw32\include/opencv2/core/version.hpp \
- C:\opencv_2.4.0_mingw32\include/opencv2/core/operations.hpp \
- C:\opencv_2.4.0_mingw32\include/opencv2/core/mat.hpp \
- C:\opencv_2.4.0_mingw32\include/opencv2/imgproc/imgproc.hpp \
- C:\opencv_2.4.0_mingw32\include/opencv2/imgproc/types_c.h \
- C:\opencv_2.4.0_mingw32\include/opencv2/core/core_c.h \
- C:\opencv_2.4.0_mingw32\include/opencv2/highgui/highgui.hpp \
- C:\opencv_2.4.0_mingw32\include/opencv2/highgui/highgui_c.h
+dft.d dft.o: ../dft.cpp C:\opencv\include/opencv2/core/core.hpp \
+ C:\opencv\include/opencv2/core/types_c.h \
+ C:\opencv\include/opencv2/core/version.hpp \
+ C:\opencv\include/opencv2/core/operations.hpp \
+ C:\opencv\include/opencv2/core/mat.hpp \
+ C:\opencv\include/opencv2/imgproc/imgproc.hpp \
+ C:\opencv\include/opencv2/imgproc/types_c.h \
+ C:\opencv\include/opencv2/core/core_c.h \
+ C:\opencv\include/opencv2/highgui/highgui.hpp \
+ C:\opencv\include/opencv2/highgui/highgui_c.h
 
-C:\opencv_2.4.0_mingw32\include/opencv2/core/core.hpp:
+C:\opencv\include/opencv2/core/core.hpp:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/core/types_c.h:
+C:\opencv\include/opencv2/core/types_c.h:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/core/version.hpp:
+C:\opencv\include/opencv2/core/version.hpp:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/core/operations.hpp:
+C:\opencv\include/opencv2/core/operations.hpp:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/core/mat.hpp:
+C:\opencv\include/opencv2/core/mat.hpp:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/imgproc/imgproc.hpp:
+C:\opencv\include/opencv2/imgproc/imgproc.hpp:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/imgproc/types_c.h:
+C:\opencv\include/opencv2/imgproc/types_c.h:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/core/core_c.h:
+C:\opencv\include/opencv2/core/core_c.h:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/highgui/highgui.hpp:
+C:\opencv\include/opencv2/highgui/highgui.hpp:
 
-C:\opencv_2.4.0_mingw32\include/opencv2/highgui/highgui_c.h:
+C:\opencv\include/opencv2/highgui/highgui_c.h:
--- Default/subdir.mk
+++ Default/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"C:\opencv_2.4.0_mingw32\include" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"C:\opencv\include" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
