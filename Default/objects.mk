ef2b34752d30c181eaf3eab5aa17d08d74340879
kamil <kamil@r830>
2012-05-26 22:37:10 +0200

    delete of non source code files

--- Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibopencv_calib3d240 -llibopencv_contrib240 -llibopencv_core240 -llibopencv_features2d240 -llibopencv_flann240 -llibopencv_gpu240 -llibopencv_highgui240 -llibopencv_imgproc240 -llibopencv_legacy240 -llibopencv_ml240 -llibopencv_nonfree240 -llibopencv_objdetect240 -llibopencv_photo240 -llibopencv_stitching240 -llibopencv_video240 -llibopencv_videostab240
-
